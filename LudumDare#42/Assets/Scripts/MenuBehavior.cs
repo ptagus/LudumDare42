bbe6d62c1b0c10e10a918f95b3082cbc3f305ec4
ptagus <ptagusss@ya.ru>
2018-08-11 13:32:49 +0600

    Menu
    Menu Scene

---
+++ LudumDare#42/Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuBehavior : MonoBehaviour
+{
+    public void Awake()
+    {
+        VolumeController.value = SettingsManager.volume;
+    }
+    float volumeDefault = 0.5f;
+    public Slider VolumeController;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void VolumeChange()
+    {
+        SettingsManager.Volume(VolumeController.value);
+    }
+
+    public void VolumeDefault()
+    {
+        VolumeController.value = volumeDefault;
+        VolumeChange();
+    }
+}
---
+++ LudumDare#42/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SettingsManager
+{
+    public static float volume = 0.5f;
+    public static void Volume(float v)
+    {
+        volume = v;
+    }
+}
