f8a0758db66e05051a33088e92b296d00705f734
ptagus <ptagusss@ya.ru>
2018-08-11 17:34:57 +0600

    Me
    Bad

---
+++ LudumDare#42/Assets/Scripts/AsteroidsBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsBehavior : MonoBehaviour {
+
+    bool toShip;
+    Transform ShipPosition;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (toShip)
+        {
+            transform.position = Vector3.Lerp( ShipPosition.position, transform.position, 0.9f);
+        }
+	}
+
+    public void NewShipPosition(Transform t)
+    {
+        ShipPosition = t;
+        toShip = true;
+    }
+}
