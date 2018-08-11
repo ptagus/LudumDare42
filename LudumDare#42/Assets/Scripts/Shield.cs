9a024a9e4e259e88142c9d36ffcb8189c97fcdfb
ptagus <ptagusss@ya.ru>
2018-08-11 15:25:37 +0600

    my
    123

---
+++ LudumDare#42/Assets/Scripts/Shield.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [Range (0,1)]
+    public float alpha = 0;
+    float exalpha = 1;
+    public Color c;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        c.a = alpha;
+        Newalpha();
+	}
+
+    void Newalpha()
+    {
+        if (exalpha != alpha)
+        {
+            exalpha = alpha;
+            GetComponent<Renderer>().material.color = c;
+        }
+        return;
+    }
+}
